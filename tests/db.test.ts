8d3010046fd1bb0c1a5463900f7811dc8c7dc5cd
ldx <liu1764115099@gmail.com>
2026-02-05 16:00:29 +0800

    test: configure vitest globals

--- tests/db.test.ts
+++ tests/db.test.ts
@@ -3,6 +3,5 @@ import { initTestDb, runMigrations } from "./support/db";
 test("migrations create core tables", async () => {
   const db = initTestDb();
   await runMigrations(db);
-  const res = db.public.query("SELECT to_regclass('instances') as t");
-  expect(res.rows[0].t).toBe("instances");
+  expect(() => db.public.query("select 1 from instances")).not.toThrow();
 });
